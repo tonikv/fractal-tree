90a71cb1af78f793fac7046c958b410493657c65
Toni Virtanen <tonikoodaa@gmail.com>
2022-03-07 10:13:06 +0200

    Added comments

--- branch.js
+++ branch.js
@@ -1,11 +1,24 @@
-class Branch{
+/*
+    Branch class
+        
+    This describes one "branch" of the tree. It has start and end position and thickness values.
+    
+    Functions:
+    branchOut -> split this branch to new branch starting from this branches end position.
+    drawBranch -> gives this branches values for drawing.
+
+*/
+
+class Branch {
     constructor(begin, end, thickness) {
-        this.begin = begin;
-        this.end = end;
+        this.begin = begin; // Where branch starts
+        this.end = end; // Where it ends
         this.thickness = thickness;
-        this.finished = false;
+        this.finished = false; // Has branch already been branched out
     }
 
+    // Function to make to make new branches after this branch. 
+    // You can adjust rotation, length, thickness and variation of new branches.
     branchOut(rotate, length, width, angleVariation) {
         let randomVector = Vector.random2D(angleVariation);
         let direction = Vector.sub(this.end, this.begin);
@@ -16,6 +29,7 @@ class Branch{
         return new Branch(this.end, newEnd, width);
     }
 
+    // Return values from this branch to draw it.
     drawBranch() {
         const object = {
             startX: this.begin.x,
