12697ac5ff92b0da16086dd87c61d3c1d17556ad
Toni Virtanen <tonikoodaa@gmail.com>
2022-03-07 10:16:59 +0200

    Added Comments and canvas size controlled now in JS

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /*
     Fractal tree with adjustible settings
 
-
+    Settings are adjusted via sliders with oninput events. Every event regrows tree with new values and draws it.
+    Tree is generated with branch object that has function to grow new branches after old ones.
 
 */
 
-const width = 350;
-const height = 500;
+let screenWidth = screen.width - 20;
+let screenHeight = 400;
 let tree = [];
 let canvas, ctx;
 let len = 0.67;
@@ -15,12 +16,14 @@ let variation = 0;
 let rootLength = 100;
 let iterations = 6;
 
+// Get reference for sliders
 const angleSlider = document.querySelector('#angle');
 const lenSlider = document.querySelector('#len');
 const variationSlider = document.querySelector('#variation');
 const iterationsSlider = document.querySelector('#iterations');
 const rootSlider = document.querySelector('#rootLength');
 
+// Event handlers for sliders
 rootSlider.oninput = () => {
     rootLength = rootSlider.value;
     growTree(iterations, angle, len, variation);
@@ -51,33 +54,34 @@ variationSlider.oninput = () => {
     drawTree()
 }
 
-
+// Initial setup and first tree generation and draw.
 function init() {
     canvas = document.querySelector('#myCanvas');
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
     ctx = canvas.getContext('2d');
-    ctx.clearRect(0,0, width, height);
+    ctx.clearRect(0, 0, screenWidth, 400);
     ctx.strokeStyle = 'rgba(255,255,255,1)'
     growTree(iterations, angle, len, variation);
     drawTree()
 }
 
+// Generate tree with user given values
 function growTree(iterations, angle, length, variation) {
     tree = [];
     let branchWidth = 6.0;
-    let begin = new Vector(width / 2, height);
-    let end = new Vector(width / 2, height - rootLength);
+    let begin = new Vector(screenWidth / 2, screenHeight);
+    let end = new Vector(screenWidth / 2, screenHeight - rootLength);
     let root = new Branch(begin, end, branchWidth);
     tree.push(root);
-    for (let i = 0; i < iterations; i++) { 
-        for (let i = tree.length - 1; i >=0; i--) {
-            if(!tree[i].finished) {
+    for (let i = 0; i < iterations; i++) {
+        for (let i = tree.length - 1; i >= 0; i--) {
+            if (!tree[i].finished) {
                 tree.push(tree[i].branchOut(angle, length, branchWidth, variation));
                 tree.push(tree[i].branchOut(-angle, length, branchWidth, variation));
                 tree[i].finished = true;
             }
-            
+
             if (branchWidth < 0.5) {
                 branchWidth = 1;
             } else {
@@ -87,8 +91,9 @@ function growTree(iterations, angle, length, variation) {
     }
 }
 
+// Draw tree with call to branch objects drawBranch function. It gives values for drawing this branch.
 function drawTree() {
-    ctx.clearRect(0,0, width, height);
+    ctx.clearRect(0, 0, screenWidth, screenHeight);
     for (let i = 0; i < tree.length; i++) {
         let branch = tree[i].drawBranch();
         ctx.beginPath();
@@ -97,13 +102,6 @@ function drawTree() {
         ctx.lineTo(branch.endX, branch.endY);
         ctx.stroke();
     }
-}
-
-function animate() {
-    if (!drawed) {
-        branch(len);
-    }
-    requestAnimationFrame(animate);
 }
 
 window.addEventListener('load', init);
